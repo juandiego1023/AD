ff097b6071bdfadd97708e6ae5ee1ed27027652c
juandiego1023 <juandiegoariasmartin@gmail.com>
2014-12-01 08:41:15 +0100

    reflection

---
+++ PCategoria/PCategoria/Program.cs
@@ -0,0 +1,24 @@
+using Gtk;
+using MySql.Data.MySqlClient;
+using System;
+
+using SerpisAd;
+
+namespace PCategoria
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			App.Instance.DbConnection = new MySqlConnection (
+				"DataSource=localhost;Database=dbprueba;User ID=root;Password=sistemas"
+			);
+			App.Instance.DbConnection.Open ();
+
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
